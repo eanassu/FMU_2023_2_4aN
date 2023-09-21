56e54272982ac08c1b8d0b7482c3bfad16224e25
Eugênio Akihiro Nassu <eanassu@gmail.com>
2023-09-20 21:09:35 -0300

    Botão para abrir outra Activity

--- app/src/main/java/br/fmu/testesoma/MainActivity.java
+++ app/src/main/java/br/fmu/testesoma/MainActivity.java
@@ -2,6 +2,7 @@ package br.fmu.testesoma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -27,4 +28,19 @@ public class MainActivity extends AppCompatActivity {
         soma = x + y;
         editTextNumber3.setText(Integer.toString(soma));
     }
+
+    public void abrirJanelaNova( View view ) {
+        int x, y, soma;
+        x = Integer.parseInt(editTextNumber.getText().toString());
+        y = Integer.parseInt(editTextNumber2.getText().toString());
+        soma = x + y;
+
+        Intent intent = new Intent(this, MainActivity2.class);
+        intent.putExtra("soma", soma );
+        startActivity(intent);
+    }
 }
+
+
+
+
---
+++ app/src/main/java/br/fmu/testesoma/MainActivity2.java
@@ -0,0 +1,22 @@
+package br.fmu.testesoma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        textView = findViewById(R.id.textView);
+        int soma = getIntent().getIntExtra("soma", 0 );
+        textView.setText(Integer.toString(soma));
+    }
+}
+
+
+
